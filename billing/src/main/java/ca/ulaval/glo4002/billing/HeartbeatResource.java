7b49e0edfdeee19cb19e3ae665b27a5c4d219704
Eric Bonin <eric.bonin.87@gmail.com>
2017-09-18 13:12:04 -0400

    Add Heartbeat resource

---
+++ billing/src/main/java/ca/ulaval/glo4002/billing/HeartbeatResource.java
@@ -0,0 +1,22 @@
+package ca.ulaval.glo4002.billing;
+
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import ca.ulaval.glo4002.billing.dto.HeartbeatDto;
+
+@Path("/heartbeat")
+public class HeartbeatResource {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public HeartbeatDto GetHeartbeat(@QueryParam("token") String token) {
+		long timestamp = new Date().toInstant().toEpochMilli();
+		return new HeartbeatDto(timestamp, token);
+	}
+}
