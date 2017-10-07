043c24017b8876f69c3d1f9c5249fb73b3ffd7d6
BongHighlander <Pandaisall@gmail.com>
2017-10-07 01:42:37 -0400

    Don't delete content

--- billing/src/main/java/ca/ulaval/glo4002/billing/domain/submission/Submission.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/domain/submission/Submission.java
@@ -1,5 +1,10 @@
 package ca.ulaval.glo4002.billing.domain.submission;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import ca.ulaval.glo4002.billing.domain.client.DueTerm;
@@ -21,5 +26,18 @@ public class Submission {
 		this.creationDate = creationDate;
 		this.items = items;
 		this.dueTerm = dueTerm;
+	}
+
+	public Date setDate(String __date) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+		try {
+			return dateFormat.parse(__date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return new Date();
+		// courtesy to
+		// stackoverflow.com/questions/35952809/java-convert-string-in-iso-instant-format-to-date
+		// mais il doit exister de meilleures méthodes
 	}
 }
