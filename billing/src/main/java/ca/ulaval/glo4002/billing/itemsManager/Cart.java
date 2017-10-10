0fd478bc050c02accdd808960bcb5d11a9c9c6af
Eric Bonin <eric.bonin.87@gmail.com>
2017-10-09 23:10:33 -0400

    Refactored Cart.java to meet Checkstyle requirements

--- billing/src/main/java/ca/ulaval/glo4002/billing/itemsManager/Cart.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/itemsManager/Cart.java
@@ -1,43 +1,48 @@
 package ca.ulaval.glo4002.billing.itemsManager;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
 import ca.ulaval.glo4002.errorManager.ErrorNegativeItemPrice;
 import ca.ulaval.glo4002.errorManager.ErrorNegativeTotal;
 import ca.ulaval.glo4002.errorManager.ErrorStack;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Cart {
 
-	public BigDecimal total = new BigDecimal(0);
-	@JsonSerialize
-	private ArrayList<ItemForSubmission> listItems = new ArrayList<ItemForSubmission>();
+  private BigDecimal total;
+  @JsonSerialize
+  private ArrayList<ItemForSubmission> listItems;
 
-	@JsonCreator
-	public Cart() {
-		super();
-	}
+  @JsonCreator
+  public Cart() {
+    total = new BigDecimal(0);
+    listItems = new ArrayList<>();
+  }
 
-	public void addItem(ItemForSubmission itemForSubmission, ErrorStack errorList) {
-		listItems.add(itemForSubmission);
-		if (itemForSubmission.price().doubleValue() < 0)
-			errorList.addError(new ErrorNegativeItemPrice(itemForSubmission.price().doubleValue()));
-		total = new BigDecimal(total.doubleValue() + itemForSubmission.total().doubleValue());
-	}
+  public void addItem(ItemForSubmission itemForSubmission,
+                      ErrorStack errorList) {
+    listItems.add(itemForSubmission);
+    if (itemForSubmission.price().doubleValue() < 0) {
+      errorList.addError(new ErrorNegativeItemPrice(itemForSubmission.price().doubleValue()));
+    }
 
-	public void checkAllItems(ErrorStack errorList) {
-		for (ItemForSubmission item : this.listItems) {
-			item.check(errorList);
-		}
-	}
+    total = new BigDecimal(total.doubleValue() + itemForSubmission.total().doubleValue());
+  }
 
-	public BigDecimal total(ErrorStack errorList) {
-		if (this.total.doubleValue() < 0)
-			errorList.addError(new ErrorNegativeTotal(this.total.doubleValue()));
-		return this.total;
-	}
+  public void checkAllItems(ErrorStack errorList) {
+    for (ItemForSubmission item : this.listItems) {
+      item.check(errorList);
+    }
+  }
+
+  public BigDecimal total(ErrorStack errorList) {
+    if (this.total.doubleValue() < 0) {
+      errorList.addError(new ErrorNegativeTotal(this.total.doubleValue()));
+    }
+
+    return this.total;
+  }
 
 }
