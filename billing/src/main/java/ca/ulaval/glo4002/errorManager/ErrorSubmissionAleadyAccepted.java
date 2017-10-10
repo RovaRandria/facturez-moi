f704535ea7b08120df99b845aec61c91d01abe6e
BongHighlander <Pandaisall@gmail.com>
2017-10-10 05:18:35 -0400

    111

--- billing/src/main/java/ca/ulaval/glo4002/errorManager/ErrorSubmissionAleadyAccepted.java
+++
@@ -1,8 +0,0 @@
-package ca.ulaval.glo4002.errorManager;
-
-public class ErrorSubmissionAleadyAccepted extends ErrorService {
-
-	public ErrorSubmissionAleadyAccepted(long id) {
-		super("wrong status", "Invoice already accepted", "invoice");
-	}
-}
