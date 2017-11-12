46c618289964f266fc481fa50355fc349497bf72
DESKTOP-QIRUBU1\Anthony <anthony_sylvain@hotmail.ca>
2017-11-12 13:57:58 -0500

    Created factories for Payment and ReceiptDto

---
+++ billing/src/main/java/ca/ulaval/glo4002/billing/factory/ReceiptDtoFactory.java
@@ -0,0 +1,10 @@
+package ca.ulaval.glo4002.billing.factory;
+
+import ca.ulaval.glo4002.billing.domain.payments.PaymentId;
+import ca.ulaval.glo4002.billing.dto.ReceiptDto;
+
+public class ReceiptDtoFactory {
+  public ReceiptDto create(PaymentId paymentId) {
+    return new ReceiptDto(paymentId.toString(), "payments/" + paymentId.toString());
+  }
+}
--- billing/src/main/java/ca/ulaval/glo4002/billing/services/PaymentService.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/services/PaymentService.java
@@ -6,25 +6,30 @@ import ca.ulaval.glo4002.billing.domain.invoices.Invoice;
 import ca.ulaval.glo4002.billing.domain.invoices.InvoiceRepository;
 import ca.ulaval.glo4002.billing.domain.payments.Payment;
 import ca.ulaval.glo4002.billing.domain.payments.PaymentId;
-import ca.ulaval.glo4002.billing.domain.payments.PaymentMethod;
 import ca.ulaval.glo4002.billing.domain.payments.PaymentRepository;
 import ca.ulaval.glo4002.billing.dto.PaymentDto;
 import ca.ulaval.glo4002.billing.dto.ReceiptDto;
+import ca.ulaval.glo4002.billing.factory.PaymentFactory;
+import ca.ulaval.glo4002.billing.factory.ReceiptDtoFactory;
 import ca.ulaval.glo4002.billing.repository.CrmClientRepository;
 import ca.ulaval.glo4002.billing.repository.HibernateInvoiceRepository;
 import ca.ulaval.glo4002.billing.repository.HibernatePaymentRepository;
 
 public class PaymentService extends BillingService {
 
-  PaymentRepository paymentRepository;
-  InvoiceRepository invoiceRepository;
-  ClientRepository clientRepository;
+  private PaymentRepository paymentRepository;
+  private InvoiceRepository invoiceRepository;
+  private ClientRepository clientRepository;
+  private PaymentFactory paymentFactory;
+  private ReceiptDtoFactory receiptDtoFactory;
 
   public PaymentService() {
     prepareDatabase();
     this.paymentRepository = new HibernatePaymentRepository();
     this.invoiceRepository = new HibernateInvoiceRepository();
     this.clientRepository = new CrmClientRepository();
+    this.paymentFactory = new PaymentFactory();
+    this.receiptDtoFactory = new ReceiptDtoFactory();
   }
 
   public PaymentService(PaymentRepository paymentRepository, InvoiceRepository invoiceRepository,
@@ -32,17 +37,19 @@ public class PaymentService extends BillingService {
     this.paymentRepository = paymentRepository;
     this.invoiceRepository = invoiceRepository;
     this.clientRepository = clientRepository;
+    this.paymentFactory = new PaymentFactory();
+    this.receiptDtoFactory = new ReceiptDtoFactory();
   }
 
   public ReceiptDto pay(PaymentDto paymentDto) {
     Invoice invoice = invoiceRepository.findLast(paymentDto.getClientId());
     Client client = clientRepository.getClient(paymentDto.getClientId());
 
-    PaymentMethod paymentMethod = new PaymentMethod(paymentDto.getPaymentMethod().getAccount(),
-        paymentDto.getPaymentMethod().getSource());
-    Payment payment = new Payment(client, paymentDto.getAmount(), paymentMethod);
+    Payment payment = paymentFactory.create(paymentDto, client);
     PaymentId paymentId = paymentRepository.insert(payment);
-    ReceiptDto receiptDto = new ReceiptDto(paymentId.toString(), "payments/" + paymentDto.getClientId().toString());
+
+    ReceiptDto receiptDto = receiptDtoFactory.create(paymentId);
+
     return receiptDto;
   }
 }
