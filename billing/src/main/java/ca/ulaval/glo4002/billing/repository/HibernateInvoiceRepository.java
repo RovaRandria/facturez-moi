e32b9b28503387b134c58615d8115908ec7dbfbd
BongHighlander <Pandaisall@gmail.com>
2017-11-13 07:54:37 -0500

    Missing client for us3 detected via error 400.

--- billing/src/main/java/ca/ulaval/glo4002/billing/repository/HibernateInvoiceRepository.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/repository/HibernateInvoiceRepository.java
@@ -1,19 +1,23 @@
 package ca.ulaval.glo4002.billing.repository;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.RollbackException;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import ca.ulaval.glo4002.billing.domain.clients.Client;
 import ca.ulaval.glo4002.billing.domain.clients.ClientId;
 import ca.ulaval.glo4002.billing.domain.invoices.Invoice;
 import ca.ulaval.glo4002.billing.domain.invoices.InvoiceId;
 import ca.ulaval.glo4002.billing.domain.invoices.InvoiceRepository;
 import ca.ulaval.glo4002.billing.entitymanager.EntityManagerProvider;
 import ca.ulaval.glo4002.billing.exceptions.InvoiceAlreadyCreatedException;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
-
-import javax.persistence.EntityManager;
-import javax.persistence.RollbackException;
-import java.util.List;
+import ca.ulaval.glo4002.billing.exceptions.NotFoundException;
 
 public class HibernateInvoiceRepository implements InvoiceRepository {
 
@@ -54,8 +58,12 @@ public class HibernateInvoiceRepository implements InvoiceRepository {
     criteria.addOrder(Order.asc("invoice.expectedPayment"));
 
     List<Invoice> invoices = criteria.list();
-    entityManager.getTransaction().commit();
-    return invoices.get(0);
+    if (invoices.size() < 1) {
+      throw new NotFoundException(Client.class.getSimpleName(), clientId.toString());
+    } else {
+      entityManager.getTransaction().commit();
+      return invoices.get(0);
+    }
   }
 
 }
