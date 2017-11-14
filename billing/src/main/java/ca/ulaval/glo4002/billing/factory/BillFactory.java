600f2f60181519acbef3a144de91a67dd666b284
Rova <rova.randrian@gmail.com>
2017-11-13 19:46:13 -0500

    Change create Bill in BillFactory

--- billing/src/main/java/ca/ulaval/glo4002/billing/factory/BillFactory.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/factory/BillFactory.java
@@ -6,11 +6,10 @@ import ca.ulaval.glo4002.billing.dto.OrderDto;
 import ca.ulaval.glo4002.billing.repository.BillIdGenerator;
 
 public class BillFactory {
-	private ProductsFactory productsFactory;
 
 	public Bill create(OrderDto order, Client client) {
 		BillIdGenerator billIdGenerator = BillIdGenerator.getInstance();
-		productsFactory = new ProductsFactory();
+		ProductsFactory productsFactory = new ProductsFactory();
 
 		return new Bill(billIdGenerator.getId(), client, order.getDate(), order.getDueTerm(),
 				productsFactory.create(order.getProductDtos()));
